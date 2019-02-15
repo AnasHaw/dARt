4d534c3c8d15c6b57fa741298d127d6160e6a0a7
Manuel <manuelpatti@hotmail.de>
2019-02-15 22:55:19 +0100

    Unity
    Added Unity Project to GitHub

---
+++ dARt-master/Assets/Script/EmbedBehavior.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmbedBehavior : MonoBehaviour
+{
+    Rigidbody rigidB;
+    // Use this for initialization
+    void Start()
+    {
+        rigidB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        rigidB.constraints = RigidbodyConstraints.FreezeAll;
+        transform.parent = collision.transform;
+
+        if (!(KeepScore.Score < 0))
+        {
+            if (collision.gameObject.name == "vier")
+            {
+                int current = KeepScore.Score - 40;
+                if (((Mathf.Sign(current)) < 0))
+                {
+                    LastScore.Score = 40;
+                }
+                else
+                {
+                    LastScore.Score = 40;
+                    KeepScore.Score -= 40;
+                    CheckScore();
+                }
+            }
+            if (collision.gameObject.name == "sechs")
+            {
+                int current = KeepScore.Score - 60;
+                if (((Mathf.Sign(current)) < 0))
+                {
+                    LastScore.Score = 60;
+                }
+                else
+                {
+                    KeepScore.Score -= 60;
+                    LastScore.Score = 60;
+                    CheckScore();
+                }
+            }
+            if (collision.gameObject.name == "acht")
+            {
+                int current = KeepScore.Score - 80;
+                if (((Mathf.Sign(current)) < 0))
+                {
+                    LastScore.Score = 80;
+                }
+                else
+                {
+                    KeepScore.Score -= 80;
+                    LastScore.Score = 80;
+                    CheckScore();
+                }
+            }
+            if (collision.gameObject.name == "eins")
+            {
+                int current = KeepScore.Score - 100;
+                if (((Mathf.Sign(current)) <= 0))
+                {
+                    LastScore.Score = 100;
+                }
+                else
+                {
+                    KeepScore.Score -= 100;
+                    LastScore.Score = 100;
+                    CheckScore();
+                }
+            }
+        }
+    }
+
+    private void CheckScore()
+    {
+        if (KeepScore.Score == 0)
+        {
+            KeepScore.Score = 720;
+        } else if (KeepScore.Score == 20)
+        {
+            KeepScore.Score = 720;
+        }
+    }
+}
---
+++ dARt-master/Assets/Script/LastScore.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastScore : MonoBehaviour
+{
+
+    public static int Score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(700, 70, 100, 20), "Score: " + Score.ToString());
+    }
+
+}
